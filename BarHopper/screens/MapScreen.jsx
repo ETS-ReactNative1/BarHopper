a73a6a9b4e9c8b415b579ced4443984bad82e215
derobertsw <derobertsw@gmail.com>
2021-10-12 14:40:40 -0400

    add map view

--- BarHopper/screens/MapScreen.jsx
+++ BarHopper/screens/MapScreen.jsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+import MapView from 'react-native-maps';
+
 export default function MapScreen() {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Tab Two</Text>
-			<View
-				style={styles.separator}
-				lightColor="#eee"
-				darkColor="rgba(255,255,255,0.1)"
+			<MapView
+				style={styles.map}
+				initialRegion={{
+					latitude: 40.74708623964595,
+					longitude: -74.0258037865746,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421
+				}}
 			/>
-			<EditScreenInfo path="/screens/MapScreen.jsx" />
 		</View>
 	);
 }
@@ -21,16 +25,16 @@ export default function MapScreen() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center'
+	},
+	map: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height
 	},
 	title: {
 		fontSize: 20,
 		fontWeight: 'bold'
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: '80%'
 	}
 });
