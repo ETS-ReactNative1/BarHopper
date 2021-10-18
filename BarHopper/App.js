9cd1799733978a4064005cb2193ef592c38e36bc
Diaeddin Motan <dmotan@macIT009028.local>
2021-10-17 21:16:20 -0400

    Added auth stuff

--- BarHopper/App.js
+++ BarHopper/App.js
@@ -5,8 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation';
 
 import useColorScheme from './hooks/useColorScheme';
+import Amplify from '@aws-amplify/core';
+import Auth from '@aws-amplify/auth';
+import config from './aws-exports';
+Amplify.configure(config);
+import { withAuthenticator } from 'aws-amplify-react-native';
 
-export default function App() {
+const App = () => {
 	const colorScheme = useColorScheme();
 	return <Navigation colorScheme={colorScheme} />;
 }
@@ -19,3 +24,5 @@ const styles = StyleSheet.create({
 		justifyContent: 'center'
 	}
 });
+
+export default withAuthenticator(App);
