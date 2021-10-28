12435d615d50e4647b2130aee0de77821dbd9380
Diaeddin Motan <dmotan@macIT009028.local>
2021-10-27 23:32:58 -0400

    Improved auth process and account screen.

--- BarHopper/screens/AccountScreen.jsx
+++ BarHopper/screens/AccountScreen.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Pressable } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
@@ -15,9 +15,39 @@ export default function AccountScreen() {
     }
   };
 
+	const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+	const [username, setUsername] = useState('');
+
+  useEffect(() => {
+		let isMounted = true;
+    try {
+      setError(null);
+      setLoading(true);
+
+      Auth.currentAuthenticatedUser({
+        bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+      }).then(user => {
+        	setUsername(user.attributes.email);
+        	console.log(`Load additional settings for user: ${user.username}`);
+
+      }).catch(err => setError(err));
+    }
+    catch (e) {
+      setError(e);
+    }
+    finally {
+			isMounted = false;
+      setLoading(false);
+    }
+  }, []);
+
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Tab Four</Text>
+			<Text style={styles.title}>
+      {error ? `Oops... ${error}` : null}
+      {loading ? "Loading..." : `Hello ${username}`}
+		</Text>
 			<View
 				style={styles.separator}
 				lightColor="#eee"
@@ -26,8 +56,8 @@ export default function AccountScreen() {
 			 <Pressable style={styles.button} onPress={() => signOut()}>
           <Text style={styles.buttonText}>Sign out</Text>
         </Pressable>
-			<EditScreenInfo path="/screens/AccountScreen.jsx" />
 		</View>
+
 	);
 }
 
@@ -45,5 +75,11 @@ const styles = StyleSheet.create({
 		marginVertical: 30,
 		height: 1,
 		width: '80%'
+	},
+	buttonText: {
+		backgroundColor: 'black',
+		color: 'white',
+		padding: 15
+
 	}
 });
